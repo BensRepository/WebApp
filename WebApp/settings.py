c4ba6ad39db86805734ff68935ffc66b4a2949ef
Ben <thebenhodges@gmail.com>
2023-05-23 00:21:53 +0100

    staged

--- WebApp/settings.py
+++ WebApp/settings.py
@@ -16,16 +16,17 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for productionND
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-p788!0hge1fk7z%%&6=pjw9+mfboq9=6tfq=1o@5z1wvafd75d'
-
+#https://stackoverflow.com/questions/15209978/where-to-store-secret-keys-django
+#https://stackoverflow.com/questions/872565/remove-sensitive-files-and-their-commits-from-git-history
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["13.41.222.173","www.csgotools.co.uk","csgotools.co.uk"]
+ALLOWED_HOSTS = []
 #"13.41.222.173","www.csgotools.co.uk","csgotools.co.uk"
 
 
