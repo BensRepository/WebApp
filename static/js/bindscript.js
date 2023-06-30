57d1ab612be66fd20f8809d063e96ecb73dc87da
Ben <thebenhodges@gmail.com>
2023-07-01 00:01:14 +0100

    bind generator stage 2 done

--- static/js/bindscript.js
+++ static/js/bindscript.js
@@ -1,3 +1,4 @@
 function keyboardKeyClick(key){
-    alert(key)
+    document.getElementById("selectedKey").innerHTML="Selected Key: "+key
+    document.getElementById("secondTwoTitle").innerHTML="2. What type of bind do you need?"
 }
