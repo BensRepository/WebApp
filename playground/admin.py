dc7622a92873a91714ec9b861982704fd3c15ec1
Ben <thebenhodges@gmail.com>
2023-10-26 19:29:47 +0100

    survival and competitive navigation finished -

--- playground/admin.py
+++ playground/admin.py
@@ -7,6 +7,12 @@ from .models import LeaderboardCompetitiveHardMode
 from .models import LeaderboardCompetitiveExpertMode
 from .models import LeaderboardCompetitiveOhnepixelMode
 
+from .models import LeaderboardSurvivalEasyMode
+from .models import LeaderboardSurvivalMediumMode
+from .models import LeaderboardSurvivalHardMode
+from .models import LeaderboardSurvivalExpertMode
+from .models import LeaderboardSurvivalOhnepixelMode
+
 admin.site.register(Case)
 admin.site.register(Rate)
 admin.site.register(LeaderboardCompetitiveEasyMode)
@@ -14,3 +20,9 @@ admin.site.register(LeaderboardCompetitiveMediumMode)
 admin.site.register(LeaderboardCompetitiveHardMode)
 admin.site.register(LeaderboardCompetitiveExpertMode)
 admin.site.register(LeaderboardCompetitiveOhnepixelMode)
+
+admin.site.register(LeaderboardSurvivalEasyMode)
+admin.site.register(LeaderboardSurvivalMediumMode)
+admin.site.register(LeaderboardSurvivalHardMode)
+admin.site.register(LeaderboardSurvivalExpertMode)
+admin.site.register(LeaderboardSurvivalOhnepixelMode)
